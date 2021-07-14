6bf10c2077af04f6a6516dcd2c623ed29d24b35c
S.Gray <shark@goatshark.com>
2021-07-14 13:15:39 -0500

    update post blade file

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<title>My Blog</title>
+<link rel="stylesheet" type="text/css" href="/app.css">
+<body>
+    <article>
+        <?php echo $post; ?>
+
+    </article>
+    <a href="/">Go back</a>
+
+</body>
+
+</html>
